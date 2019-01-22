b2700e3ef6e138b9a4647b477bd7ff22a11e3cb5
Afur <teodorowiczartur@gmail.com>
2019-01-22 11:00:54 +0100

    iOS: Fixed issue with loading local images to PDF document

--- ios/Classes/SwiftFlutterHtmlToPdfPlugin.swift
+++ ios/Classes/SwiftFlutterHtmlToPdfPlugin.swift
@@ -29,25 +29,27 @@ public class SwiftFlutterHtmlToPdfPlugin: NSObject, FlutterPlugin{
         wkWebView.loadHTMLString(htmlFileContent, baseURL: Bundle.main.bundleURL) // load html into hidden webview
         
         urlObservation = wkWebView.observe(\.isLoading, changeHandler: { (webView, change) in
-            let convertedFileURL = PDFCreator.create(printFormatter: self.wkWebView.viewPrintFormatter())
-            let convertedFilePath = convertedFileURL.absoluteString.replacingOccurrences(of: "file://", with: "") // return generated pdf path
-            if let viewWithTag = viewControler?.view.viewWithTag(100) {
-                viewWithTag.removeFromSuperview() // remove hidden webview when pdf is generated
-                
-                // clear WKWebView cache
-                if #available(iOS 9.0, *) {
-                    WKWebsiteDataStore.default().fetchDataRecords(ofTypes: WKWebsiteDataStore.allWebsiteDataTypes()) { records in
-                        records.forEach { record in
-                            WKWebsiteDataStore.default().removeData(ofTypes: record.dataTypes, for: [record], completionHandler: {})
+            // this is workaround for issue with loading local images
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                let convertedFileURL = PDFCreator.create(printFormatter: self.wkWebView.viewPrintFormatter())
+                let convertedFilePath = convertedFileURL.absoluteString.replacingOccurrences(of: "file://", with: "") // return generated pdf path
+                if let viewWithTag = viewControler?.view.viewWithTag(100) {
+                    viewWithTag.removeFromSuperview() // remove hidden webview when pdf is generated
+                    
+                    // clear WKWebView cache
+                    if #available(iOS 9.0, *) {
+                        WKWebsiteDataStore.default().fetchDataRecords(ofTypes: WKWebsiteDataStore.allWebsiteDataTypes()) { records in
+                            records.forEach { record in
+                                WKWebsiteDataStore.default().removeData(ofTypes: record.dataTypes, for: [record], completionHandler: {})
+                            }
                         }
                     }
                 }
                 
                 // dispose WKWebView
                 self.wkWebView = nil
+                result(convertedFilePath)
             }
-            
-            result(convertedFilePath)
         })
     default:
         result(FlutterMethodNotImplemented)

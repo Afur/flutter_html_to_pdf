dc044fcbca34fda61a4b2db32c34c4ce75a2b01e
T <crazygo@users.noreply.github.com>
2020-03-28 16:30:21 +0800

    Fix crash under iOS 10.x
    Remove observer before webkit's deinit

--- ios/Classes/SwiftFlutterHtmlToPdfPlugin.swift
+++ ios/Classes/SwiftFlutterHtmlToPdfPlugin.swift
@@ -47,6 +47,7 @@ public class SwiftFlutterHtmlToPdfPlugin: NSObject, FlutterPlugin{
                 }
                 
                 // dispose WKWebView
+                self.urlObservation = nil
                 self.wkWebView = nil
                 result(convertedFilePath)
             }
